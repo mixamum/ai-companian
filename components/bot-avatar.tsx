6eac8a60659b5f105cd188b7f0a240b5a8b32c76
Untilov <960150@MacBook-Pro-2.local>
2023-09-06 14:03:04 -0400

    Added companion cards and delete functionality

---
+++ components/bot-avatar.tsx
@@ -0,0 +1,14 @@
+import { Avatar } from "@radix-ui/react-avatar";
+import { AvatarImage } from "./ui/avatar";
+
+interface BotAvatarProps {
+  src: string;
+}
+
+export const BotAvatar = ({ src }: BotAvatarProps) => {
+  return (
+    <Avatar className="h-12 w-12">
+      <AvatarImage src={src} />
+    </Avatar>
+  );
+};
