fe214be1cf96fe81b741d3d4c25df7471d1cea56
Untilov <960150@MacBook-Pro-2.local>
2023-09-06 14:12:27 -0400

    Fixed Delete Only for authenticated Users

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,6 @@
 import prismadb from "@/lib/prismadb";
 import { CompanionForm } from "./components/companion-form";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
 interface CompanianIdPageProps {
   params: {
@@ -9,10 +10,16 @@ interface CompanianIdPageProps {
 
 const CompanianIdPage = async ({ params }: CompanianIdPageProps) => {
   // Check subscription
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirectToSignIn();
+  }
 
   const companion = await prismadb.companion.findUnique({
     where: {
       id: params.companionId,
+      userId,
     },
   });
 
