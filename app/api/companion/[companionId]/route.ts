2d19ff68633dc34cbac6532c0893cca46cdc51ef
Untilov <960150@MacBook-Pro-2.local>
2023-09-07 12:11:18 -0400

    Message outline for user and ai

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -25,6 +25,7 @@ export async function PATCH(req: Request, { params }: { params: { companionId: s
         const companion = await prismadb.companion.update({
             where: {
                 id: params.companionId,
+                userId: user.id
             },
             data: {
                 categoryId,
